e28df5bd999523a1fb9a4f384a9b41d1552d928e
danovs <dsludanov@gmail.com>
2024-11-12 12:35:24 +0300

    completetask

---
+++ LockANDUnlockVirtualMemory/LockANDUnlockVirtualMemory/LockANDUnlockVirtualMemory.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void handleError(const string& message) {
+    cout << message << " Error code: " << GetLastError() << endl;
+    exit(EXIT_FAILURE);
+}
+
+int main() {
+    const SIZE_T size = 4096;
+    LPVOID vm = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_READWRITE);
+
+    if (!vm) {
+        handleError("Virtual allocation failed");
+    }
+
+    if (!VirtualLock(vm, size)) {
+        handleError("Virtual lock failed");
+    }
+
+    if (!VirtualUnlock(vm, size)) {
+        handleError("Virtual unlock failed");
+    }
+
+    if (!VirtualFree(vm, 0, MEM_RELEASE)) {
+   
+            handleError("Virtual free failed");
+    }
+
+    return 0;
+}
