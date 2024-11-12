b9949ba526c7c9b02d1af22cdc525ee69634eec3
danovs <dsludanov@gmail.com>
2024-11-12 12:39:23 +0300

    completetask

---
+++ AllocationMemorySpecAddress/AllocationMemorySpecAddress/AllocationMemorySpecAddress.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void handleError(const string& message) {
+    cout << message << " Error code: " << GetLastError() << endl;
+    exit(EXIT_FAILURE);
+}
+
+int main()
+{
+    LPVOID lp;
+    const int size = 1000;
+
+    lp = VirtualAlloc(
+        (LPVOID)0x00007ffC246C,
+        size,
+        MEM_RESERVE | MEM_COMMIT,
+        PAGE_READWRITE
+    );
+
+    if (!lp) {
+        handleError("Virtual allocation failed");
+    }
+
+    cout << "Virtual memory address: " << lp << endl;
+
+    if (!VirtualFree(lp, 0, MEM_RELEASE)) {
+        handleError("Memory release failed.");
+    }
+    return 0;
+}
